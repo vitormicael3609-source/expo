2ad34ca20759309ab9392b43597609657d60e873
Alan Hughes <alan@expo.io>
2025-09-30 10:33:48 +0100

    [updates] Improve bspatch safety

--- packages/expo-updates/vendor/bsdiff/bspatch.c
+++ packages/expo-updates/vendor/bsdiff/bspatch.c
@@ -32,10 +32,27 @@ __FBSDID("$FreeBSD: src/usr.bin/bsdiff/bspatch/bspatch.c,v 1.1 2005/08/06 01:59:
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <err.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
+
+struct bspatch_context {
+	FILE *cpf, *dpf, *epf;
+	BZFILE *cpfbz2, *dpfbz2, *epfbz2;
+	u_char *old, *new;
+	int cbz2err, dbz2err, ebz2err;
+};
+
+static void cleanup_context(struct bspatch_context *ctx) {
+	if (ctx->cpfbz2) BZ2_bzReadClose(&ctx->cbz2err, ctx->cpfbz2);
+	if (ctx->dpfbz2) BZ2_bzReadClose(&ctx->dbz2err, ctx->dpfbz2);
+	if (ctx->epfbz2) BZ2_bzReadClose(&ctx->ebz2err, ctx->epfbz2);
+	if (ctx->cpf) fclose(ctx->cpf);
+	if (ctx->dpf) fclose(ctx->dpf);
+	if (ctx->epf) fclose(ctx->epf);
+	if (ctx->old) free(ctx->old);
+	if (ctx->new) free(ctx->new);
+}
 
 static off_t offtin(u_char *buf)
 {
@@ -55,26 +72,24 @@ static off_t offtin(u_char *buf)
 	return y;
 }
 
-int main(int argc,char * argv[])
+int bspatch_main(int argc, char * argv[])
 {
-	FILE * f, * cpf, * dpf, * epf;
-	BZFILE * cpfbz2, * dpfbz2, * epfbz2;
-	int cbz2err, dbz2err, ebz2err;
+	FILE * f;
+	struct bspatch_context ctx = {0};
 	int fd;
 	ssize_t oldsize,newsize;
 	ssize_t bzctrllen,bzdatalen;
 	u_char header[32],buf[8];
-	u_char *old, *new;
 	off_t oldpos,newpos;
 	off_t ctrl[3];
 	off_t lenread;
 	off_t i;
 
-	if(argc!=4) errx(1,"usage: %s oldfile newfile patchfile\n",argv[0]);
+	if(argc!=4) return 1;
 
 	/* Open patch file */
 	if ((f = fopen(argv[3], "r")) == NULL)
-		err(1, "fopen(%s)", argv[3]);
+		return 1;
 
 	/*
 	File format:
@@ -92,94 +107,126 @@ int main(int argc,char * argv[])
 
 	/* Read header */
 	if (fread(header, 1, 32, f) < 32) {
-		if (feof(f))
-			errx(1, "Corrupt patch\n");
-		err(1, "fread(%s)", argv[3]);
+		fclose(f);
+		return 1;
 	}
 
 	/* Check for appropriate magic */
-	if (memcmp(header, "BSDIFF40", 8) != 0)
-		errx(1, "Corrupt patch\n");
+	if (memcmp(header, "BSDIFF40", 8) != 0) {
+		fclose(f);
+		return 1;
+	}
 
 	/* Read lengths from header */
 	bzctrllen=offtin(header+8);
 	bzdatalen=offtin(header+16);
 	newsize=offtin(header+24);
-	if((bzctrllen<0) || (bzdatalen<0) || (newsize<0))
-		errx(1,"Corrupt patch\n");
+	if((bzctrllen<0) || (bzdatalen<0) || (newsize<0)) {
+		fclose(f);
+		return 1;
+	}
 
 	/* Close patch file and re-open it via libbzip2 at the right places */
 	if (fclose(f))
-		err(1, "fclose(%s)", argv[3]);
-	if ((cpf = fopen(argv[3], "r")) == NULL)
-		err(1, "fopen(%s)", argv[3]);
-	if (fseeko(cpf, 32, SEEK_SET))
-		err(1, "fseeko(%s, %lld)", argv[3],
-		    (long long)32);
-	if ((cpfbz2 = BZ2_bzReadOpen(&cbz2err, cpf, 0, 0, NULL, 0)) == NULL)
-		errx(1, "BZ2_bzReadOpen, bz2err = %d", cbz2err);
-	if ((dpf = fopen(argv[3], "r")) == NULL)
-		err(1, "fopen(%s)", argv[3]);
-	if (fseeko(dpf, 32 + bzctrllen, SEEK_SET))
-		err(1, "fseeko(%s, %lld)", argv[3],
-		    (long long)(32 + bzctrllen));
-	if ((dpfbz2 = BZ2_bzReadOpen(&dbz2err, dpf, 0, 0, NULL, 0)) == NULL)
-		errx(1, "BZ2_bzReadOpen, bz2err = %d", dbz2err);
-	if ((epf = fopen(argv[3], "r")) == NULL)
-		err(1, "fopen(%s)", argv[3]);
-	if (fseeko(epf, 32 + bzctrllen + bzdatalen, SEEK_SET))
-		err(1, "fseeko(%s, %lld)", argv[3],
-		    (long long)(32 + bzctrllen + bzdatalen));
-	if ((epfbz2 = BZ2_bzReadOpen(&ebz2err, epf, 0, 0, NULL, 0)) == NULL)
-		errx(1, "BZ2_bzReadOpen, bz2err = %d", ebz2err);
+		return 1;
+	if ((ctx.cpf = fopen(argv[3], "r")) == NULL)
+		return 1;
+	if (fseeko(ctx.cpf, 32, SEEK_SET)) {
+		cleanup_context(&ctx);
+		return 1;
+	}
+	if ((ctx.cpfbz2 = BZ2_bzReadOpen(&ctx.cbz2err, ctx.cpf, 0, 0, NULL, 0)) == NULL) {
+		cleanup_context(&ctx);
+		return 1;
+	}
+	if ((ctx.dpf = fopen(argv[3], "r")) == NULL) {
+		cleanup_context(&ctx);
+		return 1;
+	}
+	if (fseeko(ctx.dpf, 32 + bzctrllen, SEEK_SET)) {
+		cleanup_context(&ctx);
+		return 1;
+	}
+	if ((ctx.dpfbz2 = BZ2_bzReadOpen(&ctx.dbz2err, ctx.dpf, 0, 0, NULL, 0)) == NULL) {
+		cleanup_context(&ctx);
+		return 1;
+	}
+	if ((ctx.epf = fopen(argv[3], "r")) == NULL) {
+		cleanup_context(&ctx);
+		return 1;
+	}
+	if (fseeko(ctx.epf, 32 + bzctrllen + bzdatalen, SEEK_SET)) {
+		cleanup_context(&ctx);
+		return 1;
+	}
+	if ((ctx.epfbz2 = BZ2_bzReadOpen(&ctx.ebz2err, ctx.epf, 0, 0, NULL, 0)) == NULL) {
+		cleanup_context(&ctx);
+		return 1;
+	}
 
 	if(((fd=open(argv[1],O_RDONLY,0))<0) ||
 		((oldsize=lseek(fd,0,SEEK_END))==-1) ||
-		((old=malloc(oldsize+1))==NULL) ||
+		((ctx.old=malloc(oldsize+1))==NULL) ||
 		(lseek(fd,0,SEEK_SET)!=0) ||
-		(read(fd,old,oldsize)!=oldsize) ||
-		(close(fd)==-1)) err(1,"%s",argv[1]);
-	if((new=malloc(newsize+1))==NULL) err(1,NULL);
+		(read(fd,ctx.old,oldsize)!=oldsize) ||
+		(close(fd)==-1)) {
+		cleanup_context(&ctx);
+		return 1;
+	}
+	if((ctx.new=malloc(newsize+1))==NULL) {
+		cleanup_context(&ctx);
+		return 1;
+	}
 
 	oldpos=0;newpos=0;
 	while(newpos<newsize) {
 		/* Read control data */
 		for(i=0;i<=2;i++) {
-			lenread = BZ2_bzRead(&cbz2err, cpfbz2, buf, 8);
-			if ((lenread < 8) || ((cbz2err != BZ_OK) &&
-			    (cbz2err != BZ_STREAM_END)))
-				errx(1, "Corrupt patch\n");
+			lenread = BZ2_bzRead(&ctx.cbz2err, ctx.cpfbz2, buf, 8);
+			if ((lenread < 8) || ((ctx.cbz2err != BZ_OK) &&
+			    (ctx.cbz2err != BZ_STREAM_END))) {
+				cleanup_context(&ctx);
+				return 1;
+			}
 			ctrl[i]=offtin(buf);
 		};
 
 		/* Sanity-check */
-		if(newpos+ctrl[0]>newsize)
-			errx(1,"Corrupt patch\n");
+		if(newpos+ctrl[0]>newsize) {
+			cleanup_context(&ctx);
+			return 1;
+		}
 
 		/* Read diff string */
-		lenread = BZ2_bzRead(&dbz2err, dpfbz2, new + newpos, ctrl[0]);
+		lenread = BZ2_bzRead(&ctx.dbz2err, ctx.dpfbz2, ctx.new + newpos, ctrl[0]);
 		if ((lenread < ctrl[0]) ||
-		    ((dbz2err != BZ_OK) && (dbz2err != BZ_STREAM_END)))
-			errx(1, "Corrupt patch\n");
+		    ((ctx.dbz2err != BZ_OK) && (ctx.dbz2err != BZ_STREAM_END))) {
+			cleanup_context(&ctx);
+			return 1;
+		}
 
 		/* Add old data to diff string */
 		for(i=0;i<ctrl[0];i++)
 			if((oldpos+i>=0) && (oldpos+i<oldsize))
-				new[newpos+i]+=old[oldpos+i];
+				ctx.new[newpos+i]+=ctx.old[oldpos+i];
 
 		/* Adjust pointers */
 		newpos+=ctrl[0];
 		oldpos+=ctrl[0];
 
 		/* Sanity-check */
-		if(newpos+ctrl[1]>newsize)
-			errx(1,"Corrupt patch\n");
+		if(newpos+ctrl[1]>newsize) {
+			cleanup_context(&ctx);
+			return 1;
+		}
 
 		/* Read extra string */
-		lenread = BZ2_bzRead(&ebz2err, epfbz2, new + newpos, ctrl[1]);
+		lenread = BZ2_bzRead(&ctx.ebz2err, ctx.epfbz2, ctx.new + newpos, ctrl[1]);
 		if ((lenread < ctrl[1]) ||
-		    ((ebz2err != BZ_OK) && (ebz2err != BZ_STREAM_END)))
-			errx(1, "Corrupt patch\n");
+		    ((ctx.ebz2err != BZ_OK) && (ctx.ebz2err != BZ_STREAM_END))) {
+			cleanup_context(&ctx);
+			return 1;
+		}
 
 		/* Adjust pointers */
 		newpos+=ctrl[1];
@@ -187,19 +234,29 @@ int main(int argc,char * argv[])
 	};
 
 	/* Clean up the bzip2 reads */
-	BZ2_bzReadClose(&cbz2err, cpfbz2);
-	BZ2_bzReadClose(&dbz2err, dpfbz2);
-	BZ2_bzReadClose(&ebz2err, epfbz2);
-	if (fclose(cpf) || fclose(dpf) || fclose(epf))
-		err(1, "fclose(%s)", argv[3]);
+	BZ2_bzReadClose(&ctx.cbz2err, ctx.cpfbz2);
+	BZ2_bzReadClose(&ctx.dbz2err, ctx.dpfbz2);
+	BZ2_bzReadClose(&ctx.ebz2err, ctx.epfbz2);
+	ctx.cpfbz2 = NULL;
+	ctx.dpfbz2 = NULL;
+	ctx.epfbz2 = NULL;
+	if (fclose(ctx.cpf) || fclose(ctx.dpf) || fclose(ctx.epf)) {
+		cleanup_context(&ctx);
+		return 1;
+	}
+	ctx.cpf = NULL;
+	ctx.dpf = NULL;
+	ctx.epf = NULL;
 
 	/* Write the new file */
 	if(((fd=open(argv[2],O_CREAT|O_TRUNC|O_WRONLY,0666))<0) ||
-		(write(fd,new,newsize)!=newsize) || (close(fd)==-1))
-		err(1,"%s",argv[2]);
+		(write(fd,ctx.new,newsize)!=newsize) || (close(fd)==-1)) {
+		cleanup_context(&ctx);
+		return 1;
+	}
 
-	free(new);
-	free(old);
+	free(ctx.new);
+	free(ctx.old);
 
 	return 0;
 }
